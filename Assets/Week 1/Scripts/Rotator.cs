309174b65db965ff672e179d676a1758c9db995f
Casiana <atkicasi@sheridancollege.ca>
2024-01-10 14:26:55 -0500

    Task 1 Minute Hand movement - minute hand is moving

--- Assets/Week 1/Scripts/Rotator.cs
+++ Assets/Week 1/Scripts/Rotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float speed = 30.0f;
+    public float speed = 360.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, -speed * Time.deltaTime); 
+        transform.Rotate(0, 0, 1); 
     }
 }
