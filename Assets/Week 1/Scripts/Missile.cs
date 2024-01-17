892784b858b6eb9c43ae7267ae26c63ea4a1e2cb
atkicasi <atkicasi@sheridancollege.ca>
2024-01-17 14:42:54 -0500

    ***Had to close and reopen the project, that's why there are so many changes. I just hope this doesn't make everything corrupt or something along those lines. Added collision between the missile and the player, the player moves just not very well.

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     Rigidbody2D rigibody;
-    float speed = 5.0f;
+    float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
