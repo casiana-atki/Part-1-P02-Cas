a43a9b0faf0c8e4058199224cc46fb458a5fb029
atkicasi <atkicasi@sheridancollege.ca>
2024-01-17 14:14:44 -0500

    Player is moving on keyboard inputs. Wall and collision created as well.

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    Rigidbody2D rigibody;
     float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigibody = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigibody.MovePosition(rigibody.position + direction); 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("missile collision");
+        Destroy(gameObject);
     }
 }
