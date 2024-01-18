76260edce0f3e84ca7abe5d48bffa71c77676abb
Casiana Atkinson <atkicasi@shernet.sheridancollege.ca>
2024-01-18 14:33:47 -0500

    Week 2 Task 4B Completed

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (door.activeInHierarchy == true)
+        {
+            door.SetActive(false);
+        }
+        else if
+            (door.activeInHierarchy == false)
+      { 
+            door.SetActive(true);
+        }
+
+        
+    }
+}
