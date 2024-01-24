ca089ad9716191832ba0ec42769b1747341396a6
atkicasi <atkicasi@sheridancollege.ca>
2024-01-24 14:57:09 -0500

    Task 3 & 4A Completed

--- Assets/Week 3/Script/Car_Behaviour.cs
+++ Assets/Week 3/Script/Car_Behaviour.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Car_Behaviour : MonoBehaviour
 {
     float accelaration;
     float steering;
+    //I made the max speed smaller since I made the car smaller and it seemed to accelerate faster. 
+    public float maxSpeed = 350; 
     public float forwardSpeed = 500;
     public float steeringSpeed = 100;
-    Rigidbody2D rigidbody; 
+    Rigidbody2D rigibody; 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigibody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,8 +26,11 @@ public class Car_Behaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
-        Vector2 force = transform.up * accelaration * forwardSpeed * Time.deltaTime; 
-        rigidbody.AddForce(force);
+        rigibody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * accelaration * forwardSpeed * Time.deltaTime;
+        if (rigibody.velocity.magnitude < maxSpeed)
+        { 
+        rigibody.AddForce(force);
+    }
     }
 }
